4b18bcaa27c79a9281511891a99cdc9579756c34
Rofie Pratama <rofihbm12345@gmail.com>
2026-05-09 19:50:21 +0700

    fixed php medical_records migration

--- database/migrations/2026_04_02_135901_create_medical_records_table.php
+++ database/migrations/2026_04_02_135901_create_medical_records_table.php
@@ -10,23 +10,21 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('medical_records', function (Blueprint $table) {
-            Schema::create('medical_record', function (Blueprint $table) {
-                $table->uuid('id')->primary();
+{
+    Schema::create('medical_records', function (Blueprint $table) {
+        $table->uuid('id')->primary();
 
-                $table->uuid('user_id');
-                $table->foreign('user_id')->references('id')->on('users');
+        $table->uuid('user_id');
+        $table->foreign('user_id')->references('id')->on('users');
 
-                $table->foreignId('rumah_sakit_id')
-                    ->constrained('rumah_sakit');
+        $table->foreignId('rumah_sakit_id')
+            ->constrained('rumah_sakit');
 
-                $table->string('dokter');
-                $table->text('keluhan');
-                $table->timestamp('created_at')->useCurrent();
-            });
-        });
-    }
+        $table->string('dokter');
+        $table->text('keluhan');
+        $table->timestamp('created_at')->useCurrent();
+    });
+}
 
     /**
      * Reverse the migrations.
